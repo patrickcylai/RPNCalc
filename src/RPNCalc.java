8b70cd10a449c525d205380d40743c40a7ac94db
Patrick Lai <patrickcylai@gmail.com>
2015-11-25 15:48:23 +1100

    Improved functionality
    Added some new functionality and some error handling

--- src/RPNCalc.java
+++ src/RPNCalc.java
@@ -1,29 +1,39 @@
 /**
- * Created by patricklai on 25/11/2015.
+ * Basic Reverse Polish Notation Calculator
+ * Created by Patrick Lai on 25/11/2015.
  */
 import java.util.*;
 
 public class RPNCalc {
+    /*
+    Determines if the string is a numeric type
+     */
     public static boolean isNumeric(String str) {
         return str.matches("-?\\d+(\\.\\d+)?");  //match a number with optional '-' and decimal.
     }
 
+    /*
+    Displays the result
+     */
     public static void displayResult(double result) {
         System.out.println(result);
     }
 
+    /*
+    Main Calculator
+     */
     public static void main(String[] args) {
+        Scanner scanner;
+        double temp = 0;
         Stack<Double> numStack = new Stack<>();
-        Scanner cin;
-
         while (true) {
-            cin = new Scanner(System.in);
-            String str = cin.nextLine();
+            scanner = new Scanner(System.in);
+            String str = scanner.nextLine();
             String[] splitStr = str.split("\\s+");
 
             double result = 0;
-            for (int i = 0; i < splitStr.length; ++i) {
-                switch (splitStr[i]) {
+            calcLoop :for (int i = 0; i < splitStr.length; ++i) {
+                switch (splitStr[i]) { // Check whether there is a operator
                     case "+":
                         result = numStack.pop() + numStack.pop();
                         numStack.push(result);
@@ -40,14 +50,31 @@ public class RPNCalc {
                         result = numStack.pop() / numStack.pop();
                         numStack.push(result);
                         break;
-                    default:
-                        if (isNumeric(splitStr[i])) {
+                    case "ans":
+                        numStack.push(temp);
+                        break;
+                    case "stack()":
+                        System.out.println(numStack);
+                        break;
+                    case "clearstack()":
+                        numStack = new Stack<>();
+                        System.out.println("Stack Cleared");
+                        break;
+                    case "quit()":
+                        scanner.close();
+                        return;
+                    default: // If it isn't an operator
+                        if (isNumeric(splitStr[i])) { // Check if numeric so it can be pushed to stack
                             numStack.push(Double.parseDouble(splitStr[i]));
+                        } else {
+                            System.out.println(str + " is not a valid command");
+                            break calcLoop;
                         }
                         break;
                 }
             }
             displayResult(result);
+            temp = result;
         }
     }
 }
